3fbf05d6b762fcab4057f47ad067b0a5624b9ba1
jackie174 <57372321+jackie174@users.noreply.github.com>
2022-01-12 22:14:00 -0500

    Update web.py

--- web.py
+++ web.py
@@ -14,7 +14,7 @@ print("import requests" + "\n" + "out = requests.get('http://www.google.com/')"
         + "\n" + "with open('filename.txt', 'wb') as r:"+ "\n" + "  r.write(out.content)")
 with open('filename.txt', 'wb') as r:
     r.write(out.content)
-)
+
     
     
     

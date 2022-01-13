49a6afe470f9a0d6f93e3e9df0d7197090920d82
jackie174 <pcheng1@ualberta.ca>
2022-01-12 22:16:22 -0500

    add webb

--- webb.py
+++ webb.py
@@ -1,4 +1,8 @@
-
+"""Cite:
+Url: https://towardsdatascience.com/how-to-download-files-using-python-ffbca63beb5c
+Author: Aaron S
+Date: Feb 5, 2020
+"""
 import requests
 
 
@@ -7,7 +11,8 @@ out = requests.get('http://www.google.com/')
 #print(out.text)
 print("import requests" + "\n" + "out = requests.get('http://www.google.com/')"
         + "\n" + "with open('filename.txt', 'wb') as r:"+ "\n" + "  r.write(out.content)")
-with open('filename.txt', 'wb') as r: 
+with open('filename.txt', 'wb') as r:
     r.write(out.content)
+
     
 
